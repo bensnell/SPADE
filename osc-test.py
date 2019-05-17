0ae98a3526f36cf66669c5fdc369120beff7216f
Ben Snell <bensnell@localprojects.com>
2019-05-17 18:30:31 -0400

    Attempt to do osc comms

--- osc-test.py
+++ osc-test.py
@@ -10,5 +10,6 @@ dis.map("/rtls", saveTrackedLocation)
 # server = osc_server.ThreadingOSCUDPServer(("127.0.0.1",8282), dis)
 # server.serve_forever()
 
+
 server = osc_server.AsyncOSCUDPServer(("127.0.0.1",8282), dis)
 transport, protocol = await server.create_serve_endpoint()
